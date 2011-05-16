5492b1d9fdf8e30983b549a711c4ae493c6e828e
Chris Hannam <bassdread@gmail.com>
2011-05-16 11:56:48 +0100

    clearing out

--- src/tontine/apps/utils/key.py
+++
@@ -1,8 +0,0 @@
-"""Provide a method to store connection details for
-the remote service"""
-
-class OauthToken(object):
-    """Hold oauth details"""
-
-class UsernameToken(object):
-    """Hold username details."""
